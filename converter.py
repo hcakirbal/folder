83024ea9f03fa1a15b11b1c04f3baa6e2a44d44b
Hasan Çakır Bal <hasancakirbal2003@gmail.com>
2024-03-16 13:34:42 +0300

    Add files via upload

---
+++ converter.py
@@ -0,0 +1,11 @@
+# 1280x720 to 1920x1080 converter
+
+original_width = int(input("enter width: \n"))
+converted_width = (original_width / 1280) * 1920
+print("converted width is ", converted_width)
+
+print("\n")
+
+original_height = int(input("enter height: \n"))
+converted_height = (original_height / 720) * 1080
+print("converted height is ", converted_height)
